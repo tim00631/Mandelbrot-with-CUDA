9a591ce6ce399f847eb3c7848bbeafac211f4dd5
“yuhao” <tim00631@gmail.com>
2020-12-23 17:36:16 +0800

    kernel3

--- kernel.cu
+++ kernel.cu
@@ -60,6 +60,14 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
     printf("%d, %d\n",g_width, g_height);
     cudaDeviceSynchronize();
     cudaMemcpy(host_img, d_img, resX * resY * sizeof(int), cudaMemcpyDeviceToHost);
+    
+    for (int j = 0; j < resX; i++) {
+        for (int i = 0; i < resY; j++) {
+            printf("%d", host_img[i][j]);
+        }
+        printf("\n");
+    }
+
     memcpy(img, host_img,resX * resY * sizeof(int));
     cudaFree(d_img);
 }
