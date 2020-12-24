a9e6b1eac12d32362128e1e93f500fee1cca692b
“yuhao” <tim00631@gmail.com>
2020-12-24 14:15:49 +0800

    kernel4

--- kernel4.cu
+++ kernel4.cu
@@ -38,14 +38,14 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
     float stepX = (upperX - lowerX) / resX;
     float stepY = (upperY - lowerY) / resY;
     cudaMalloc((void **)&d_img, resX * resY * sizeof(int));
-    // host_img = (int *) malloc(resX * resY * sizeof(int)); // kernel1
+    host_img = (int *) malloc(resX * resY * sizeof(int)); // kernel1
     // cudaHostAlloc((void **)&host_img, resX * resY * sizeof(int),cudaHostAllocDefault); // kernel2
     dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
     dim3 numBlock(resX / BLOCK_SIZE, resY / BLOCK_SIZE);
     mandelKernel<<<numBlock, blockSize>>>(d_img, lowerX, lowerY, stepX, stepY, resX, resY, maxIterations);
     
     cudaDeviceSynchronize();
-    cudaMemcpy(img, d_img, resX * resY * sizeof(int), cudaMemcpyDeviceToHost);
-    // memcpy(img,host_img,resX * resY * sizeof(int));
+    cudaMemcpy(host_img, d_img, resX * resY * sizeof(int), cudaMemcpyDeviceToHost);
+    memcpy(img,host_img,resX * resY * sizeof(int));
     cudaFree(d_img);
 }
