c11dd02dd75fac47d75f9e728bf0fdbe47bb09c1
“yuhao” <tim00631@gmail.com>
2020-12-24 15:17:30 +0800

    kernel2

--- kernel.cu
+++ kernel.cu
@@ -1,41 +1,32 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define BLOCK_SIZE 25
-__global__ void mandelKernel(
-        int* d_img, float lowerX, float lowerY, float stepX,
-        float stepY, int width, int height, int maxIterations, 
-        int g_width, int g_height, size_t pitch) {
+#define BLOCK_SIZE 16
+__global__ void mandelKernel(int* d_img, float lowerX, float lowerY, float stepX, float stepY, int width, int height, int maxIterations) 
+{
     // To avoid error caused by the floating number, use the following pseudo code
     // float x = lowerX + thisX * stepX;
     // float y = lowerY + thisY * stepY;
 
     unsigned int thisX = blockIdx.x * blockDim.x + threadIdx.x;
     unsigned int thisY = blockIdx.y * blockDim.y + threadIdx.y;
-    int end_j = thisY + g_height;
-    int end_i = thisX + g_width;
-    for (int j = thisY; j < end_j; j++)
-    {
-        for (int i = thisX; i < end_i; i++)
+    if (thisX < width && thisY < height) {
+        int idx = thisY * width + thisX;
+        float c_re = lowerX + thisX * stepX;
+        float c_im = lowerY + thisY * stepY;
+        float z_re = c_re, z_im = c_im;
+        int i = 0;
+        for (i = 0; i < maxIterations; ++i)
         {
-            if (i < width && j < height) {
-                int idx = j * width + i;
-                float c_re = lowerX + i * stepX;
-                float c_im = lowerY + j * stepY;
-                float z_re = c_re, z_im = c_im;
-                int val = 0;
-                for (val = 0; val < maxIterations; val++)
-                {
-                    if (z_re * z_re + z_im * z_im > 4.f)
-                        break;
-                    float new_re = z_re * z_re - z_im * z_im;
-                    float new_im = 2.f * z_re * z_im;
-                    z_re = c_re + new_re;
-                    z_im = c_im + new_im;
-                }
-                d_img[idx] = val;
-            }
+            if (z_re * z_re + z_im * z_im > 4.f)
+                break;
+
+            float new_re = z_re * z_re - z_im * z_im;
+            float new_im = 2.f * z_re * z_im;
+            z_re = c_re + new_re;
+            z_im = c_im + new_im;
         }
+        d_img[idx] = i;
     }
 }
 
@@ -52,12 +43,10 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
     cudaHostAlloc((void **)&host_img, resX * resY * sizeof(int),cudaHostAllocDefault); // kernel2
     dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
     dim3 numBlock(resX / BLOCK_SIZE, resY / BLOCK_SIZE);
-    int g_width = numBlock.x / BLOCK_SIZE;
-    int g_height = numBlock.y / BLOCK_SIZE;
- 
-    mandelKernel<<<numBlock, blockSize>>>(d_img, lowerX, lowerY, stepX, stepY, resX, resY, maxIterations, g_width, g_height, pitch);
+    mandelKernel<<<numBlock, blockSize>>>(d_img, lowerX, lowerY, stepX, stepY, resX, resY, maxIterations);
+    
     cudaDeviceSynchronize();
     cudaMemcpy(host_img, d_img, resX * resY * sizeof(int), cudaMemcpyDeviceToHost);
-    memcpy(img, host_img,resX * resY * sizeof(int));
+    memcpy(img,host_img,resX * resY * sizeof(int));
     cudaFree(d_img);
 }
